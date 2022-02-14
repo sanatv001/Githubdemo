cca5daeeee6d7b63fdbdec7cdcf4ca61b93c584b
sanatvishwakarma <sanatvishwakarma@sanats-air>
2022-02-14 19:31:19 +0530

    Second changes in featurs

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import java.util.Iterator;
+
 public class Student {
 
 	public static void main(String[] args) {
 		System.out.println("List of students");
-
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		}
 	}
 
 }
